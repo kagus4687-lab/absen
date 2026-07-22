f8485098df08761b98bbae26b0b1c352d6462aa7
kagus4687-lab <kagus4687@gmail.com>
2026-07-22 08:36:39 +0800

    Create page.tsx

---
+++ src/app/announcements/page.tsx
@@ -0,0 +1,12 @@
+// src/app/announcements/page.tsx
+export default function AnnouncementsPage() {
+  return (
+    <div>
+      <h1>Latest Announcements</h1>
+      <ul>
+        <li>Announcement 1: New semester starts soon!</li>
+        <li>Announcement 2: School holiday schedule updated.</li>
+      </ul>
+    </div>
+  );
+}
