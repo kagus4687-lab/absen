50cf53d49e1b74cd4a10a539f29ba6940fb5e9dd
kagus4687-lab <kagus4687@gmail.com>
2026-07-22 08:34:41 +0800

    Create page.tsx

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+// src/app/about/page.tsx
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Information about the school or organization goes here.</p>
+    </div>
+  );
+}
